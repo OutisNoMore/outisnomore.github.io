c48a8fea3f958c72e5f63e06b8ab5ec291bb37f5
OutisNoMore <65424540+OutisNoMore@users.noreply.github.com>
2021-01-08 15:01:00 -0800

    Update translator.js

--- javascript/translator.js
+++ javascript/translator.js
@@ -1,36 +1,3 @@
-const context = new (window.AudioContext || window.webkitAudioContext)();
-
-async function makeSound(sequence){
-  const [dot, dash] = await fetchBuffer();
-  let time = 0;
-  sequence.split('').forEach(type => {
-    if(type === ' '){
-      time += 0.3;//wait 300 milliseconds after every letter
-      return;
-    }
-    let test = context.createBufferSource();
-    test.buffer = type === '-' ? dash : dot;
-    test.connect(context.destination);
-    test.start(context.currentTime + time);
-    time += test.buffer.duration + 0.03;//makes sure order is synchronous with 30 millisecond gap
-  });
-  setTimeout(function(){
-    talking = false;
-  }, time*1000);
-}
-
-
-function fetchBuffer(){
-  return Promise.all([
-    '../dot.wav',
-    '../dash05.wav'
-  ].map(url => fetch(url)
-   .then(response => response.arrayBuffer())
-   .then(buffer => context.decodeAudioData(buffer))
-   )
-  );
-}
-
 let morseToLet = {
   ".-": "a",
   "-...": "b",
