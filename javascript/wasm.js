ae7ec149ebb68951953c46e4f75d31a98dd157c4
OutisNoMore <OutisNoMore@protonmail.com>
2022-09-26 09:23:26 -0700

    update wasm

--- javascript/wasm.js
+++ javascript/wasm.js
@@ -1,13 +1,17 @@
 let fibo = Module.cwrap('fibo', 'number', ['number']);
 const BTN = document.getElementById('fibo');
 const INDEX = document.getElementById('index');
-const OUTPUT = document.getElementById('output');
+const TABLE = document.getElementById('sequence');
 
 BTN.addEventListener("click", () => {
   let index = +INDEX.value;
   let sequence = "";
+  let row, cell1, cell2;
   for(let i = 0; i < index; i++){
-    sequence += fibo(i) + " ";
+    row = TABLE.insertRow();
+    cell1 = row.insertCell();
+    cell2 = row.insertCell();
+    cell1.innerHTML = i;
+    cell2.innerHTML = fibo(i);
   }
-  OUTPUT.innerHTML = sequence;
 });
