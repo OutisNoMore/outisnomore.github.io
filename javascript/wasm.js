5a18d744f10ecbbd01b74af09555b0992e2bf8ed
OutisNoMore <OutisNoMore@protonmail.com>
2022-09-28 20:40:43 -0700

    Pad tables

--- javascript/wasm.js
+++ javascript/wasm.js
@@ -3,15 +3,16 @@ const BTN = document.getElementById('fibo');
 const INDEX = document.getElementById('index');
 const TABLE = document.getElementById('sequence');
 
-BTN.addEventListener("click", () => {
-  let index = +INDEX.value;
-  let sequence = "";
-  let row, cell1, cell2;
+const printFibo = function(){
+  let index = +INDEX.value; // Get value from input as number
+  let row, cell1, cell2;    // Handlers to build table with fibonacci sequence
   for(let i = 0; i < index; i++){
     row = TABLE.insertRow();
     cell1 = row.insertCell();
     cell2 = row.insertCell();
     cell1.innerHTML = i;
     cell2.innerHTML = fibo(i);
-  }
-});
+  }  
+}
+
+BTN.addEventListener("click", printFibo);
