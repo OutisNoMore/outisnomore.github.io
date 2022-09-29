0c8f781304f872060b8162c88146c10b96cd7db2
OutisNoMore <OutisNoMore@protonmail.com>
2022-09-28 21:53:04 -0700

    Fix bug when deleting rows

--- javascript/wasm.js
+++ javascript/wasm.js
@@ -5,13 +5,13 @@ const TABLE = document.getElementById('sequence');
 const ERROR_OOB = "ERROR: Please enter a number between 0 and 46";
 
 const printFibo = function(){
-  let index = +INDEX.value; // Get value from input as number
-  if(index >= 0 && index <= 46){
+  let index = +INDEX.value + 1; // Get value from input as number
+  if(index >= 1 && index <= 47){
     let row, cell1, cell2, i; // Handlers to build table with fibonacci sequence
-    if((TABLE.rows.length - 1) < (index + 1)){
-      // Add more rows to table	
+    if((TABLE.rows.length - 1) < index){
+      // Add more rows to table
       i = TABLE.rows.length - 1;
-      for(; i < (index + 1); i++){
+      for(; i < index; i++){
         row = TABLE.insertRow();
         cell1 = row.insertCell();
         cell2 = row.insertCell();
@@ -21,7 +21,7 @@ const printFibo = function(){
     }
     else if((TABLE.rows.length - 1) > index){
       // delete rows from table	
-      while((TABLE.rows.length-1) !== index){
+      while((TABLE.rows.length - 1) !== index){
         TABLE.deleteRow(TABLE.rows.length - 1);
       }	
     }
@@ -29,7 +29,7 @@ const printFibo = function(){
   else{
     // Index is out of bounds
     alert(ERROR_OOB);
-    console.log("You entered: (" + index + ") which is not between 0 and 46");
+    console.log("You entered: (" + (index - 1) + ") which is not between 0 and 46");
   }
 }
 
