05eb9a2e269bd6ddb4f46e7e4163083c400a2664
OutisNoMore <OutisNoMore@protonmail.com>
2022-10-17 20:46:29 -0700

    add to projects and resume

--- javascript/wasm.js
+++ javascript/wasm.js
@@ -1,4 +1,4 @@
-let fibo = Module.cwrap('fibo', 'number', ['number']);
+let fibo = Module.cwrap('fibo', 'BigInt', ['BigInt']);
 const BTN = document.getElementById('fibo');
 const INDEX = document.getElementById('index');
 const TABLE = document.getElementById('sequence');
@@ -6,7 +6,7 @@ const ERROR_OOB = "ERROR: Please enter a number between 0 and 46";
 
 const printFibo = function(){
   let index = +INDEX.value + 1; // Get value from input as number
-  if(index >= 1 && index <= 47){
+  //if(index >= 1 && index <= 47){
     let row, cell1, cell2, i; // Handlers to build table with fibonacci sequence
     if((TABLE.rows.length - 1) < index){
       // Add more rows to table
@@ -25,12 +25,12 @@ const printFibo = function(){
         TABLE.deleteRow(TABLE.rows.length - 1);
       }	
     }
-  }
+/*  }
   else{
     // Index is out of bounds
     alert(ERROR_OOB);
     console.log("You entered: (" + (index - 1) + ") which is not between 0 and 46");
-  }
+  }*/
 }
 
 BTN.addEventListener("click", printFibo);
--- project/WASM/hello.c
+++ project/WASM/hello.c
@@ -7,12 +7,12 @@
 #define EXTERN
 #endif
 
-EXTERN EMSCRIPTEN_KEEPALIVE int fibo(int index){
+EXTERN EMSCRIPTEN_KEEPALIVE unsigned int fibo(int index){
   if(index == 0 || index == 1){
     return index;
   }
-  int a = 0;
-  int b = 1;
+  unsigned int a = 0;
+  unsigned int b = 1;
   while(index > 1){
     b = a + b;
     a = b - a;
